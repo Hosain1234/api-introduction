c71965b361a87abf83054c45a30e9de3150a24cc
hosain1234 <fahim7787@gmail.com>
2022-02-26 02:36:03 +0600

    fetch kore json file to array korte pari

---
+++ js/JSON.js
@@ -0,0 +1,20 @@
+const user = {
+    name: 'mokhles',
+    address: 'paradice',
+    job: 'job-less',
+    password: '$mokhles'
+}
+const stringified = JSON.stringify(user);
+// console.log(user);
+// console.log(stringified);
+
+const shop = {
+    name: 'dream shop',
+    address: 'when you are sleeping',
+    products: [ 'Allah er sathe dekha', 'jannat', 'fame', 'beautiful hur'],
+    isPossible: true
+}
+const dreamStringify = JSON.stringify(shop);
+console.log(dreamStringify);
+const convertedIntoRegularArray = JSON.parse(dreamStringify);
+console.log(convertedIntoRegularArray);
--- js/app.js
+++ js/app.js
@@ -1,20 +1,15 @@
-const user = {
-    name: 'mokhles',
-    address: 'paradice',
-    job: 'job-less',
-    password: '$mokhles'
-}
-const stringified = JSON.stringify(user);
-// console.log(user);
-// console.log(stringified);
-
-const shop = {
-    name: 'dream shop',
-    address: 'when you are sleeping',
-    products: [ 'Allah er sathe dekha', 'jannat', 'fame', 'beautiful hur'],
-    isPossible: true
-}
-const dreamStringify = JSON.stringify(shop);
-console.log(dreamStringify);
-const convertedIntoRegularArray = JSON.parse(dreamStringify);
-console.log(convertedIntoRegularArray);
+function loadData(){
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
+};
+function tenUser(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(xyz => console.log(xyz))
+};
+function akshowPost(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => console.log(data))
+};
