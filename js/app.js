3464cb8b51f8054125c1b4775560846792854991
hosain1234 <fahim7787@gmail.com>
2022-02-25 22:27:56 +0600

    JSON stringify ...make an object string and reverse a string to normal js

--- js/app.js
+++ js/app.js
@@ -4,4 +4,17 @@ const user = {
     job: 'job-less',
     password: '$mokhles'
 }
-console.log(user);
+const stringified = JSON.stringify(user);
+// console.log(user);
+// console.log(stringified);
+
+const shop = {
+    name: 'dream shop',
+    address: 'when you are sleeping',
+    products: [ 'Allah er sathe dekha', 'jannat', 'fame', 'beautiful hur'],
+    isPossible: true
+}
+const dreamStringify = JSON.stringify(shop);
+console.log(dreamStringify);
+const convertedIntoRegularArray = JSON.parse(dreamStringify);
+console.log(convertedIntoRegularArray);
